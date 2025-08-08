6e31392f00975153189e6f55ebd7dfeff45078d1
Joao Lucas <jldecristo@gmail.com>
2025-08-07 21:13:27 -0300

    construtotres

--- src/entities/Circle.java
+++ src/entities/Circle.java
@@ -1,7 +1,15 @@
 package entities;
 
-public class Circle extends Shape{
+public class Circle extends Shape {
 
     private Double radius;
+
+    public Circle() {
+    }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
 
 }
