fffba73ccb5c692c726ca50c2d64565c6fc4213a
Joao Lucas <jldecristo@gmail.com>
2025-08-07 21:02:32 -0300

    construtores

--- src/entities/Rectangle.java
+++ src/entities/Rectangle.java
@@ -5,4 +5,13 @@ public class Rectangle extends Shape {
     private Double width;
     private Double height;
 
+    public Rectangle() {
+    }
+
+    public Rectangle(Color color, Double width, Double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
 }
